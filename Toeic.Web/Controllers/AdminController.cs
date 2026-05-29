2849b00493d3eade5cc83bec339be6c7334126ad
Biển Xanh <84816418+thesea58@users.noreply.github.com>
2026-05-29 23:35:04 +0700

    Day 9: Add admin question listing page

--- Toeic.Web/Controllers/AdminController.cs
+++ Toeic.Web/Controllers/AdminController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Toeic.Infrastructure.Identity;
+using Toeic.Infrastructure.Persistence;
 
 namespace Toeic.Web.Controllers;
 
 [Authorize(Roles = AppRoles.Admin)]
 public class AdminController : Controller
 {
+	private readonly AppDbContext _dbContext;
+
+	public AdminController(AppDbContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+
 	public IActionResult Index()
 	{
 		return View();
+	}
+
+	[HttpGet("/Admin/Questions")]
+	public async Task<IActionResult> Questions()
+	{
+		var questions = await _dbContext.Questions
+			.AsNoTracking()
+			.OrderBy(q => q.Id)
+			.ToListAsync();
+
+		return View(questions);
 	}
 }
