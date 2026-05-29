2a4f206934e2c9b79418370ce61b666e8cd9babb
Biển Xanh <84816418+thesea58@users.noreply.github.com>
2026-05-29 23:49:04 +0700

    Day 17: Add vocabulary flashcards

--- Toeic.Web/Controllers/VocabularyController.cs
+++ Toeic.Web/Controllers/VocabularyController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Toeic.Infrastructure.Persistence;
+using Toeic.Web.Models.Vocabulary;
 
 namespace Toeic.Web.Controllers;
 
@@ -24,5 +25,39 @@ public class VocabularyController : Controller
 			.ToListAsync();
 
 		return View(entries);
+	}
+
+	[HttpGet("/Vocabulary/Flashcards")]
+	public async Task<IActionResult> Flashcards(int index = 0, bool showBack = false)
+	{
+		var entries = await _dbContext.VocabularyEntries
+			.AsNoTracking()
+			.OrderBy(v => v.Topic)
+			.ThenBy(v => v.Word)
+			.ToListAsync();
+
+		if (!entries.Any())
+		{
+			return View("NoFlashcards");
+		}
+
+		if (index < 0)
+		{
+			index = 0;
+		}
+		if (index >= entries.Count)
+		{
+			index = 0;
+		}
+
+		var vm = new FlashcardViewModel
+		{
+			Entry = entries[index],
+			Index = index,
+			Total = entries.Count,
+			ShowBack = showBack
+		};
+
+		return View(vm);
 	}
 }
---
+++ Toeic.Web/Models/Vocabulary/FlashcardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Toeic.Domain.Entities;
+
+namespace Toeic.Web.Models.Vocabulary;
+
+public class FlashcardViewModel
+{
+	public VocabularyEntry Entry { get; set; } = null!;
+	public int Index { get; set; }
+	public int Total { get; set; }
+	public bool ShowBack { get; set; }
+}
